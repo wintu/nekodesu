50a9409a2228b060f58ac8d093e789b617df67ca
wintu <wintu520@gmail.com>
2018-10-18 09:25:01 +0900

    とりあえず構成

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,10 +1,22 @@
-/* eslint no-console:0 */
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
-//
-// To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
-// layout file, like app/views/layouts/application.html.erb
+import Vue from 'vue'
+import Router from 'vue-router'
+import App from './App.vue'
 
-console.log('Hello World from Webpacker')
+Vue.use(Router)
+
+const routes = [
+  { path: '/', component: () => import(/* webpackChunkName: "HelloWorld" */ './components/HelloWorld.vue') }
+]
+
+const router = new Router({
+  mode: 'history',
+  routes
+})
+
+document.addEventListener('DOMContentLoaded', () => {
+  new Vue({
+    el: '#app',
+    router,
+    render: (h) => h(App),
+  })
+})
