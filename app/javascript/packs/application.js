6a96a3c004933ee8ea6fe413996ffe35e564f310
wintu <wintu520@gmail.com>
2018-10-19 17:13:54 +0900

    create base

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,11 +1,36 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Axios from 'axios'
 import App from './App.vue'
 
 Vue.use(Router)
 
+Vue.mixin({
+  methods: {
+    async ApiGet (endPoint) {
+      try {
+        const res = await Axios.get(endPoint)
+        return res.data
+      } catch (e) {
+        return e.response ? e.message.data : { error: e.response }
+      }
+    },
+
+    async ApiPost (endPoint, params) {
+      try {
+        const token = document.getElementsByTagName('meta').find(meta => meta.name === 'csrf-token')
+        params.authenticity_token = token.content
+        const res = await Axios.post(endPoint, params)
+        return res.data
+      } catch (e) {
+        return e.response ? e.response.data : { error: e.response }
+      }
+    }
+  }
+})
+
 const routes = [
-  { path: '/', component: () => import(/* webpackChunkName: "HelloWorld" */ './components/HelloWorld.vue') }
+  { path: '/', component: require('./components/HelloWorld.vue').default }
 ]
 
 const router = new Router({
@@ -17,6 +42,6 @@ document.addEventListener('DOMContentLoaded', () => {
   new Vue({
     el: '#app',
     router,
-    render: (h) => h(App),
+    render: (h) => h(App)
   })
 })
